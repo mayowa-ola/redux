0ea87adbf1f6dc4ceab3cb7678b41d230fd0405b
mayowa_ola <mayowaoladunjoye@gmail.com>
2020-06-20 15:23:22 +0100

    applying middleware to redux

--- src/index.js
+++ src/index.js
@@ -8,23 +8,35 @@ const store = configureStore();
 //     console.log('State changed', store.getState());
 // });
 
+// store.dispatch((dispatch,getState)=> {
+//     //call an api
+//     //if resolve, dispatch
+//     dispatch({type:'bugReceived', bugs: [1,2,3,4]});
+//     console.log(getState)
+//     //if reject log error
+// })
 
-store.dispatch(addUsers({name:'User 1'}));
-store.dispatch(addUsers({name:'User 2'}));
-store.dispatch(bugAdded({description:'Bug 1'}));
-store.dispatch(bugAdded({description:'Bug 2'}));
-store.dispatch(bugAdded({description:'Bug 3'}));
-store.dispatch(bugAssignToUser({bugId:1, userId:1}));
-store.dispatch(projectAdded({name:'Project 1'}));
+store.dispatch({
+    type: "error",
+    payload: {message:"An error occured"}
+});
+
+// store.dispatch(addUsers({name:'User 1'}));
+// store.dispatch(addUsers({name:'User 2'}));
+// store.dispatch(bugAdded({description:'Bug 1'}));
+// store.dispatch(bugAdded({description:'Bug 2'}));
+// store.dispatch(bugAdded({description:'Bug 3'}));
+// store.dispatch(bugAssignToUser({bugId:1, userId:1}));
+// store.dispatch(projectAdded({name:'Project 1'}));
 
 
-// // unsubscribe();
+// // // unsubscribe();
 
-store.dispatch(bugResolved({id:2}));
-// store.dispatch(bugRemoved({id:1}));
+// store.dispatch(bugResolved({id:2}));
+// // store.dispatch(bugRemoved({id:1}));
 
-const unResolved = getUnresolvedBugs(store.getState());
-const bugs = getBugsAssignedTouser(1)(store.getState());
-console.log(bugs);
+// const unResolved = getUnresolvedBugs(store.getState());
+// const bugs = getBugsAssignedTouser(1)(store.getState());
+// console.log(bugs);
 
 console.log(store.getState());
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -11,10 +11,18 @@
 
 //USiNG REDUX TOOL KIT
 
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore,getDefaultMiddleware} from '@reduxjs/toolkit';
 // import reducer from './bugs';
 import reducer from './combineReducer';
+import logger from './middleware/logger';
+import func from './middleware/func';
+import toast from './middleware/toast';
 
 export default function() {
-   return configureStore({reducer});
+   return configureStore({
+      reducer,
+      middleware: [
+         ...getDefaultMiddleware(),
+         logger("console"), toast, func]
+   });
 }
